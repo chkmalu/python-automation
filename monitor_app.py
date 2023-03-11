be8fc009faa1440ea4194e728c10bd3cad89ffeb
malu <chkmalu@gmail.com>
2023-03-11 23:07:26 +0100

    update

---
+++ monitor_app.py
@@ -0,0 +1,34 @@
+import requests
+import smtplib
+import os
+
+url = 'http://44.203.31.191:8080'
+get_status = requests.get(url).status_code
+
+def send_mail(txt):
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as sm:
+        sm.login(os.environ['EMAIL_NAME'],os.environ['EMAIL_PWD'])
+        ms = txt
+        sm.sendmail(os.environ['EMAIL_NAME'], os.environ['EMAIL_NAME'], ms)
+try:
+    if get_status == 200:
+        print("application is up and running")
+    else:
+        print(f"applicatio is down with error code {get_status}")
+        msg = "Subject: testing\nthe application is down with please restart"
+        send_mail(msg)
+except Exception as expt:
+    print(f"applicatoin not reachable with error {expt}")
+    msg = "can reach application kindly restart the server"
+    send_mail(msg)
+
+
+
+
+
+
+
+# with smtplib.SMTP_SSL('smtp.gmail.com', 465) as sm:
+#         sm.login('chkmalu@gmail.com','przvgrjsyhhztinr')
+#         msg = "Subject: testing\nthe application is down with please restart"
+#         sm.sendmail('chkmalu@gmail.com', 'chkmalu@gmail.com', msg)
