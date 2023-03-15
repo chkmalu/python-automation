62d04aec38a8911e8d41d4e997e34f9d5710b12e
malu <chkmalu@gmail.com>
2023-03-15 10:25:00 +0100

    update

--- backup.py
+++ backup.py
@@ -1,7 +1,10 @@
 import boto3
+import schedule
+import time
 
 client = boto3.client('ec2')
 
+#filter for a single volume, remove the filter to capture all volumes
 get_vol = client.describe_volumes(Filters=[
         {
             'Name': 'tag:Name',
@@ -9,24 +12,32 @@ get_vol = client.describe_volumes(Filters=[
         },
     ],)['Volumes']
 
+def backup():
+    for vol in get_vol:
+        vol_tag = (vol['Tags'])
+        #get the volume the tag to tag th snapshot
+        for tag in vol_tag:
+            name = tag['Key']
+            value = tag['Value']
+        vol_id = vol['Attachments']
+        #get the volume id to create snapshot
+        for id in vol_id:
+            snap = client.create_snapshot(VolumeId=id['VolumeId'],TagSpecifications=[
+            {
+                'ResourceType': 'snapshot',
+                'Tags': [
+                    {
+                        'Key': name,
+                        'Value': value
+                    },
+                ]
+            },
+        ])
+            print(f"{value} snapshot created")
 
 
-for vol in get_vol:
-    vol_tag = (vol['Tags'])
-    for tag in vol_tag:
-        name = tag['Key']
-        value = tag['Value']
-    vol_id = vol['Attachments']
-    for id in vol_id:
-        snap = client.create_snapshot(VolumeId=id['VolumeId'],TagSpecifications=[
-        {
-            'ResourceType': 'snapshot',
-            'Tags': [
-                {
-                    'Key': name,
-                    'Value': value
-                },
-            ]
-        },
-    ])
-        print(snap)
+schedule.every().saturday.at("12:00").do(backup)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
--- snapshots_cleanup.py
+++ snapshots_cleanup.py
@@ -1,11 +1,16 @@
 import boto3
 from operator import itemgetter
+import schedule
+import time
 
 client = boto3.client('ec2')
 
-get_snapshots = client.describe_snapshots(OwnerIds=['self'])['Snapshots']
-sort_by_date = sorted(get_snapshots, key=itemgetter('StartTime'),reverse=True)
+def snap():
+    get_snapshots = client.describe_snapshots(OwnerIds=['self'])['Snapshots']
+    sort_by_date = sorted(get_snapshots, key=itemgetter('StartTime'),reverse=True)
 
-for snap in sort_by_date[2]:
-    print(f"Removing snap id:{snap['SnapshotId']}")
-    client.delete_snapshot(SnapshotId=snap['SnapshotId'],)
+    for snap in sort_by_date[2]:
+        print(f"Removing snap id:{snap['SnapshotId']}")
+        client.delete_snapshot(SnapshotId=snap['SnapshotId'],)
+
+snap()
